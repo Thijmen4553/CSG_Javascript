a6099b9e30e2533aa2f83890a7c56ad30f0d6bea
Thijmen4553 <144799569+Thijmen4553@users.noreply.github.com>
2024-11-26 15:01:13 +0000

    bob

---
+++ onze game/JS/mijnGame.js
@@ -0,0 +1,44 @@
+function preload() {
+    achtergrondmuziek = loadSound("sounds/bensound-groovyhiphop.mp3");
+    foto1 = loadImage("images/achtergrondzand.jpg");
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    
+    textFont("Monospace");
+    textSize(44);
+    textAlign(CENTER,CENTER);  
+    frameRate(50);
+    spel = new Spacers();
+    spel.nieuwSpel();
+}
+
+function draw() {
+  background(foto1);
+    if (spel.actief && !spel.levelGehaald) {
+        spel.update();
+    }
+    spel.teken();
+}
+
+function keyTyped() {
+  if (!spel.actief && !spel.levelGehaald) {
+    // begin spel
+    spel.actief = true;
+    achtergrondmuziek.loop();
+  }
+  if ((spel.levelGehaald && !spel.afgelopen) && keyCode == ENTER) {
+    // level gehaald tijdens het spel
+    spel.nieuwLevel();
+  }
+  if ((spel.afgelopen) && keyCode == 32) {
+    // einde spel => 32 = ENTER 
+    achtergrondmuziek.stop();
+    spel.nieuwSpel();
+  }  
+}
